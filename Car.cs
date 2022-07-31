49ad4aca47caf65bd2b1e161e00b35cccb09daf6
Lusin <71445814+lusinagad@users.noreply.github.com>
2022-07-31 04:03:09 +0400

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPexamples
+{
+    public class Car
+    {
+        public string ManufacturerName;
+        
+        public int speed;
+        public void PrintManufacturerName()
+        {
+            Console.WriteLine($"Car manufacturer Name is: {ManufacturerName} ");
+        }
+      
+        public void PrintSpeed()
+        {
+            Console.WriteLine($"Speed is: {speed}");
+        }
+
+    }
+}
