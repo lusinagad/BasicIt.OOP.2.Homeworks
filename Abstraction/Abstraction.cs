3a29d9e0fd20405ddae85ac888070181badf624f
Lusin <71445814+lusinagad@users.noreply.github.com>
2022-08-07 22:05:22 +0400

    Add files via upload

---
+++ Abstraction/Abstraction.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstraction
+{
+    abstract class Transport
+    {      
+        public  string Name { get; } 
+        public Transport(string name)  
+        {
+            Name = name;
+        }
+        public abstract void Move();   // абстрактный метод
+        public abstract int Speed { get; set; }  // абстрактное свойство
+    }
+    class Ship : Transport
+    {
+        public  Ship(string name) : base(name){} 
+        public override void Move()
+        {
+            Console.WriteLine($"{Name}  is swim");
+        }
+
+        int speed;
+        public override int Speed 
+        {
+            get => speed;
+            set => speed = value;
+            
+        }
+    }   
+    class Aircraft : Transport 
+    {
+        public Aircraft(string name) : base(name) { }
+        public override void Move()
+        {
+            Console.WriteLine($"{Name} is fly");
+        }      
+        public override int Speed
+        {
+            get; set;
+        }
+    }
+   
+    class Car : Transport
+    {
+        public Car(string name) : base(name) { }
+        public override void Move()
+        {
+            Console.WriteLine($"{Name} car is move");
+        }
+
+        int speed;
+        public override int Speed
+        {
+            get => speed;
+            set => speed = value;
+        }
+    }
+}
