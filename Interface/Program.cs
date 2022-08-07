8056a368378ea1650fb74290e743b63cdab745ef
Lusin <71445814+lusinagad@users.noreply.github.com>
2022-08-07 20:29:36 +0400

    Create Program.cs

---
+++ Interface/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace InterfacesExample
+{
+    class Program
+    {
+
+    
+        static void Main(string[] args)
+        {
+            Transport transport = new Transport();
+            electrictransport electrictransport = new electrictransport();
+
+            transport.Type();
+           
+            electrictransport.Type();
+                   
+        }
+    }
+}
