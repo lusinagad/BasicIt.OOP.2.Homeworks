933debb4735a6c0f596ce31c90000bb1ec49b2da
Lusin <71445814+lusinagad@users.noreply.github.com>
2022-08-07 20:34:05 +0400

    Add files via upload

---
+++ Interface/Transport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesExample
+{
+   public  class Transport : IMovable
+    {
+        public   void Type()
+        {
+            Console.WriteLine("General transport");
+        }      
+    }
+    public class electrictransport : Transport, IMovable
+    {
+        public new  void Type()
+        {
+            Console.WriteLine("Types of electrical trasnport are cars, buses, trains, trams");
+        }
+    }
+}
